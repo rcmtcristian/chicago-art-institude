466f2ddc0ad93a34b2859e7c58db40ca17ca871c
rcmtcristian <rcmtcristian@gmail.com>
2022-08-11 13:34:28 -0400

    changed to images

--- js/main.js
+++ js/main.js
@@ -1,27 +1,50 @@
 
-document.querySelector('button').addEventListener('click', getFetch)
- let id = document.getElementById('info').value
-function getFetch() {  
-  // const card = document.querySelector('card').value
-  const url = `https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number
-`
+// document.querySelector('button').addEventListener('click', getFetch)
+//  let id = document.getElementById('code')
+// let title = document.getElementById('searchTitle').value
+// function getFetch() {  
+//   // const card = document.querySelector('card').value
+//   const url = `https://api.artic.edu/api/v1/artworks?fields=${id},title,artist_display,date_display,main_reference_number`
  
+//   fetch(url)
+//     .then(res => res.json()) // parse response as JSON
+//     .then(data => {
+//       
+//       // let cont = document.getElementById('cont')
+//       // let image = data.data[0].api_link
+//       let titleIn = document.getElementById('titleIn')
+//       id.textContent = data.data[0].id
+//       // let date = document.querySelector('#date')
+
+//       titleIn.textContent = data.data[0].title
+//       // cont.textContent = data.data[0].artist_display
+//       // date.innerText = data.data[0].date_display
+//       
+//       console.log(data.data[0])
+//     })
+//     .catch(err => {
+//       console.log(`error ${err}`)
+//     });
+// }
+
+document.querySelector('button').addEventListener('click', getFetch)
+// let id = document.getElementById('code')
+
+let title = document.getElementById('searchTitle')
+function getFetch() {
+  // const card = document.querySelector('card').value
+  const url = `https://www.artic.edu/iiif/2/{identifier}/full/843,/0/default.jpg`
+
   fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      // let pix = document.getElementById('card')
-      // let cont = document.getElementById('cont')
-      // let image = data.data[0].api_link
-      // let titleIn = document.getElementById('titleIn')
-      // let date = document.querySelector('#date')
-
-      // titleIn.textContent = data.data[0].artist_title
-      // cont.textContent = data.data[0].artist_display
-      // date.innerText = data.data[0].date_display
-      // pix.src = image
+      let identifier = data.data[0].image_id
+ let pix = document.getElementById('card')
+      pix.src = identifier
       console.log(data)
     })
     .catch(err => {
       console.log(`error ${err}`)
     });
 }
+
